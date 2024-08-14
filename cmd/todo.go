91e7f9716fbaaa809c7cab1cb0f0bde49b064262
Hustini <ejdersalih02@gmail.com>
2024-08-14 17:44:14 +0200

    changed the description of the input command

--- cmd/todo.go
+++ cmd/todo.go
@@ -9,7 +9,7 @@ var inputCmd = &cobra.Command{
 	Use:   "input",
 	Short: "get input",
 	Long: "get input" +
-		"For example: TodoApp input -i 5",
+		"For example: TodoApp input -i Hello World",
 
 	Run: input,
 }
