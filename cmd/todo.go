986be520c47cf85f0022e582acb73eae7015ece6
Hustini <ejdersalih02@gmail.com>
2024-08-15 19:15:27 +0200

    added a test function to test if the cmd is being recognized and added new cmd showAll

--- cmd/todo.go
+++ cmd/todo.go
@@ -8,11 +8,17 @@ import (
 	_ "os"
 )
 
+func test(cmd *cobra.Command, args []string) {
+	fmt.Println("Approved")
+}
+
 func init() {
 	// add command to root
 	rootCmd.AddCommand(inputCmd)
+	rootCmd.AddCommand(showAllCmd)
 	// Flag here and Flag in input have to be named the same
 	inputCmd.Flags().StringP("message", "i", "default", "Get a input from the user")
+	showAllCmd.Flags().StringP("", "", "", "")
 }
 
 var inputCmd = &cobra.Command{
@@ -27,4 +33,13 @@ var inputCmd = &cobra.Command{
 func input(cmd *cobra.Command, args []string) {
 	message, _ := cmd.Flags().GetString("message")
 	fmt.Println(message)
+}
+
+var showAllCmd = &cobra.Command{
+	Use:   "showAll",
+	Short: "Show all",
+	Long: "Shows all stored tasks" +
+		"For example: TodoApp showAll",
+
+	Run: test,
 }
